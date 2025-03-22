33b32ef859fdafe6d64b3102aa10ed356f0d9260
Aniket Rathore <aniketrathore911@gmail.com>
2025-03-22 14:40:18 +0530

    Driver Entity Added

---
+++ src/main/java/com/aniket/project/uber/uberApp/entities/Driver.java
@@ -0,0 +1,22 @@
+package com.aniket.project.uber.uberApp.entities;
+
+import jakarta.persistence.*;
+import org.locationtech.jts.geom.Point;
+
+@Entity
+public class Driver {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private User user;
+
+    private Double rating;
+
+    private Boolean available;
+    @Column(columnDefinition = "Geometry(Point, 4326)")
+    Point currentLocation;
+
+
+}
